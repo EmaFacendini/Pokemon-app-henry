6cfb93b042cc1278e9c1221e53e52cbbf74f280e
EmanuelFacendini <efacendini@gmail.com>
2024-02-19 00:05:41 -0300

    frontcasi

--- client/src/Components/LandingPage/LandingPage.jsx
+++ client/src/Components/LandingPage/LandingPage.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import style from './LandingPage'
-import logo from "../image/logo.png"
+import './LandingPage.css'
 import {Link} from "react-router-dom"
+import logo from "../image/logo.png"
+import pokebolaini from "../image/pokebolaini.png"
 
 
 const LandingPage = () => {
@@ -9,14 +10,17 @@ const LandingPage = () => {
         <div className="LandingPage">
           
 
-            <div className={style.tittle} >
-                <img src={logo} alt="welcome to my pokemon's application" />
+            <div className="tittle" >
+                <img src={logo} alt="welcome to my pokemon's application" className="createdBy" />
             </div>
-            <Link to = "/home">
-                <button className="buttonImage" ><img src="" alt="" /></button>
-            </Link>
-
-            <img src="" alt="pokeApi By Ema Facendini" className={style.createdBy} />
+            
+           
+            <Link to="/home" className="imageButton">
+    <img src={pokebolaini} alt="" />
+</Link>
+          
+            <img src="https://i.pinimg.com/originals/66/89/dc/6689dc331be27e66349ce9a4d15ddff3.gif" alt="pokeApi By Ema Facendini" className="pikachuRun" />
+            
 
           
         </div>
